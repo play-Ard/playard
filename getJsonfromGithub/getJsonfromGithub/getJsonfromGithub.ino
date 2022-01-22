17ee91eeab4fdacc48681d41c56ce6c2e0d3b937
Erhan <80072131+erhan-namli@users.noreply.github.com>
2022-01-22 20:12:44 +0300

    some adjustments

--- getJsonfromGithub/getJsonfromGithub/getJsonfromGithub.ino
+++ getJsonfromGithub/getJsonfromGithub/getJsonfromGithub.ino
@@ -2,17 +2,21 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
 // Declaration for SSD1306 display connected using software I2C (default case):
 #define SCREEN_WIDTH 128  
 #define SCREEN_HEIGHT 64  
 #define OLED_RESET -1     //- if your screen has no reset pin, you have to change that value to -1
 
+// Wifi and HTTP libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-#include <ArduinoJson.h>
-const char* ssid = "RobustCoffeeShop";
-const char* password = "robust9496";
+
+#include <ArduinoJson.h>  // to parse the resulting data.
+
+const char* ssid = "WiFi_ssid";
+const char* password = "WiFi_password";
 
 HTTPClient http;
 
@@ -41,7 +45,7 @@ void loop() {
       WiFiClient client;
       HTTPClient http;
 
-    http.begin(client, "http://my-json-server.typicode.com/erhan-namli/gameduino/db");  //Specify request destination
+    http.begin(client, "http://play-ard.herokuapp.com/questions/4");  //Specify request destination
     int httpCode = http.GET();                                  //Send the request
 
     if (httpCode > 0) { //Check the returning code
@@ -49,27 +53,33 @@ void loop() {
         DynamicJsonDocument doc(400); //For ESP32/ESP8266 you'll mainly use dynamic.
 
         DeserializationError error = deserializeJson(doc, client);
-
+        String json = http.getString();
         if (!error) {
-          String deneme = doc["questions"][0]["question"]; 
-         
-          Serial.print("------");
-          Serial.print(deneme);
-          Serial.print("------");
+          String question = doc["questions"]; 
+          String answer = doc["answers"]; 
+
           display.clearDisplay();
           display.setTextSize(1);
           display.setTextColor(WHITE);
           display.setCursor(0, 10);
-          display.println(deneme);
+          display.println("--------");
+          display.println(question);
+          display.println("--------");
+          display.println(answer);
           display.display();
 
+          Serial.print("------");
+          Serial.print(question);
+          Serial.print(answer);
+          Serial.print("------");
+          
           
   } else {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.f_str());
     return;
   }
-      String json = http.getString();   //Get the request response payload
+         //Get the request response payload
     }
     http.end();   //Close connection
   }
