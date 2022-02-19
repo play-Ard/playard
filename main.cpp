59964df97b087f8ce5c3da85adebbb4aaa0d82ef
Erhan <80072131+erhan-namli@users.noreply.github.com>
2022-02-19 19:19:31 +0000

    testing

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Your First C++ Program
+
+#include <iostream>
+#include "modules/ui/Point.h"
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
---
+++ tests/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdint.h>
+using namespace std;
+
+int main(){
+
+  uint16_t falan=4;
+  cout<< "Erhan";
+  cout<< falan;
+  return 0;
+}
