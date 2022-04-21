6b7666c665fed64e673f060017a894e4f5f976a3
Siyabend Ürün <urunsiyabend@gmail.com>
2022-04-21 16:43:44 +0300

    REF: Collect core modules into one header file PlayardCore.h

---
+++ src/main/core/PlayardCore.h
@@ -0,0 +1,12 @@
+#ifndef PLAYARD_CORE_H
+#define PLAYARD_CORE_H
+
+#include "../Game.h"
+#include "../Input.h"
+#include "../GFXEngine.h"
+#include "../Data.h"
+#include "../SceneManager.h"
+#include "../Scene.h"
+
+
+#endif
